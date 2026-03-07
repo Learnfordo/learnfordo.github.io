404b70b79a2d5830d22cecf0284b1ffb9ce76ec6
高佳强 <gaojiaqiang@gaojiaqiangdeMacBook-Pro.local>
2026-03-07 13:59:23 +0800

    Initial commit: personal blog

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'jiaqiang | 个人博客';
+export const SITE_DESCRIPTION = '个人简历与技术博客';
